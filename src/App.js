3b5e6b29831121f00e660df72290326be06563fc
Harsamerveer <harsamerveersingh@gmail.com>
2024-12-23 11:22:36 -0800

    Updated App.js routing structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Hero from "./components/Hero"; 
 import Services from "./components/Services";
-import AboutUs  from "./components/AboutUs";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs"; // Import AboutUs component
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Hero />
-      <Services />
-      <AboutUs />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Hero />} />  {/* Main Home Page */}
+        <Route path="/about" element={<AboutUs />} />  {/* AboutUs Page */}
+        <Route path="/services" element={<Services />} />  {/* Services Page */}
+        <Route path="/contact" element={<Contact />} />  {/* Contact Page */}
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
 export default App;
+  
