55aed3218e9821a65c67c4bafc5e7f0a2ef76bb0
Harsamerveer <harsamerveersingh@gmail.com>
2024-12-23 11:23:39 -0800

    Updated navbar.js from now on it will have services, contact and about section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link component
 
 function Navbar() {
-  const handleScroll = (e, target) => {
-    e.preventDefault();
-    const element = document.getElementById(target);
-    element.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#home">RiskEvall</a>
+        <a className="navbar-brand" href="/">RiskEvall</a>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,15 +15,18 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-        <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#contact" onClick={(e) => handleScroll(e, "contact")}>Contact</a>
+              <Link className="nav-link" to="/services">Services</Link> {/* Link to Services page */}
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#aboutus" onClick={(e) => handleScroll(e, "aboutus")}>About</a>
+              <Link className="nav-link" to="/contact">Contact</Link> {/* Link to Contact page */}
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link> {/* Link to AboutUs page */}
             </li>
           </ul>
         </div>
